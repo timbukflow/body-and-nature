11bd53635f376671c35b56fc1f80f55090843b35
Ivo Schwizer <ivoschwizer@gmail.com>
2024-04-01 12:30:32 +0200

    up

---
+++ index.php
@@ -0,0 +1,72 @@
+<!-- 
+
+Made by Schwizer Design GmbH
+Say hello @ schwizerdesign.ch
+Version 1.0
+
+-->
+
+<!DOCTYPE html>
+<html lang="de">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
+
+    <meta charset="UTF-8">
+    <title>Body and nature - Stephanie Meier</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="In ihrer Praxis in St.Gallen bietet Stephanie Meier eine Vielfalt an Massagetherapien, darunter klassische Massage, Dorn-Therapie, Schwangerschaftsmassage, Manuelle Schmerztherapie MST und Myofasciale Triggerpunkt-Therapie. Sie kombiniert traditionelle und spezialisierte Methoden, um individuelle Bedürfnisse für Entspannung und Schmerzlinderung zu erfüllen.">
+    <meta name="keywords" content="Klassische Massage, Dorn-Therapie, Schwangerschaftsmassage, Manuelle Schmerztherapie MST, Myofasciale Triggerpunkt-Therapie">
+
+    <meta name="author" content="Body and nature - Stephanie Meier">
+    <link rel="canonical" href="https://bodyandnature.ch/" />
+
+    <meta name="robots" content="index, follow" /> 
+    <meta http-equiv="cache-control" content="public, max-age=3600" />
+    <meta http-equiv="pragma" content="cache" />
+
+    <link rel="icon" type="image/svg+xml" href="/img/favicon.svg">
+    <link rel="icon" type="image/png" href="/img/favicon.png">
+
+    <meta property="og:title" content="Body and nature - Stephanie Meier">
+    <meta property="og:description" content="In ihrer Praxis in St.Gallen bietet Stephanie Meier eine Vielfalt an Massagetherapien, darunter klassische Massage, Dorn-Therapie, Schwangerschaftsmassage, Manuelle Schmerztherapie MST und Myofasciale Triggerpunkt-Therapie. Sie kombiniert traditionelle und spezialisierte Methoden, um individuelle Bedürfnisse für Entspannung und Schmerzlinderung zu erfüllen.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://bodyandnature.ch">
+    <meta property="og:image" content="https://bodyandnature.ch/img/og-image.jpg">
+    <meta property="og:locale" content="de_CH">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Body and nature - Stephanie Meier">
+    <meta name="twitter:description" content="In ihrer Praxis in St.Gallen bietet Stephanie Meier eine Vielfalt an Massagetherapien, darunter klassische Massage, Dorn-Therapie, Schwangerschaftsmassage, Manuelle Schmerztherapie MST und Myofasciale Triggerpunkt-Therapie. Sie kombiniert traditionelle und spezialisierte Methoden, um individuelle Bedürfnisse für Entspannung und Schmerzlinderung zu erfüllen.">
+    <meta name="twitter:image" content="https://bodyandnature.ch/img/twitter-image.jpg">
+
+    <meta name="format-detection" content="telephone=yes">
+    <meta property="business:contact_data:street_address" content="Buchstrasse 27">
+    <meta property="business:contact_data:locality" content="St.Gallen"> 
+    <meta property="business:contact_data:region" content="9000">
+    <meta property="business:contact_data:postal_code" content="9000">
+    <meta property="business:contact_data:country_name" content="Switzerland">
+    
+    <link rel="stylesheet" href="main.css" />
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="main.js"></script>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet">   
+
+</head>
+
+<body>
+    <div class="logo">
+        <img src="img/logo.svg" alt="logo">
+    </div>
+
+    <footer>
+        <p>
+            Stephanie Meier, Buchstrasse 27, 9000 St.Gallen <br>
+            Telefon 078 642 06 35, <a href="mailto:praxis@bodyandnature.ch">praxis@bodyandnature.ch</p>
+    </footer>
+   
+</body>
+</html>
